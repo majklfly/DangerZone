083760d907a428206715877ead1bc46440aa7b3a
majklfly <majklfly@gmail.com>
2020-04-18 09:26:32 +0100

    testing circleci

--- src/__mocks__/axios.js
+++
@@ -1,8 +0,0 @@
-export default {
-  get: jest.fn(() => Promise.resolve({ data: {} })),
-  create: jest.fn(),
-  defaults: {
-    xsrfHeaderName: "X-CSRFTOKEN",
-    xsrfCookieName: "csrftoken"
-  }
-};
