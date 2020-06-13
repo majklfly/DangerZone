7a78c4e1ad36f6b9b7f92962b7b0a3e9f181341c
majklfly <majklfly@gmail.com>
2020-06-13 15:06:31 +0100

    deploy 2, fixed redux

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -2,10 +2,11 @@ import thunk from "redux-thunk";
 import allReducers from "./reducers";
 import { createStore, compose, applyMiddleware } from "redux";
 
+let devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 export const store = createStore(
   allReducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  undefined,
+  compose(applyMiddleware(thunk), devTools)
 );
