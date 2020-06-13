aeca95c1f2289448055fc9c7669c368f4cb31ea5
majklfly <majklfly@gmail.com>
2020-06-13 15:22:39 +0100

    build 3

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -2,11 +2,14 @@ import thunk from "redux-thunk";
 import allReducers from "./reducers";
 import { createStore, compose, applyMiddleware } from "redux";
 
-let devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
+const middleware = [
+  applyMiddleware(thunk),
+  ...(window.__REDUX_DEVTOOLS_EXTENSION__
+    ? [window.__REDUX_DEVTOOLS_EXTENSION__()]
+    : [])
+];
 export const store = createStore(
   allReducers,
   undefined,
-  compose(applyMiddleware(thunk), devTools)
+  compose(...middleware)
 );
