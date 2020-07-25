5ce05198af99c7e363a63b0103061d1b49d296ca
majklfly <majklfly@gmail.com>
2020-07-25 11:05:34 +0100

    updated settings

--- user/urls.py
+++ user/urls.py
@@ -18,8 +18,10 @@ urlpatterns = [
          name='change-password'),
     path('request-reset-email/', views.RequestPasswordResetEmail.as_view(),
          name='request-reset-email'),
-    path('password-reset/<uidb64>/<token>/',
-         views.PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
     path('password-reset-complete/', views.SetNewPasswordAPIView.as_view(),
          name="password-reset-complete"),
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
+    path('password-reset/<uidb64>/<token>/',
+         views.PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+
 ]
