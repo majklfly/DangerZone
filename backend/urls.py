7f950fd334131d1213bea637edbae9ae16c92ff0
majklfly <majklfly@gmail.com>
2020-07-25 11:28:10 +0100

    updated settings

--- backend/urls.py
+++ backend/urls.py
@@ -8,7 +8,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('nested_admin/', include('nested_admin.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
     path('', include('user.urls')),
     path('', include('quiz.urls')),
     path('', include('article.urls')),
@@ -16,6 +15,7 @@ urlpatterns = [
     path('api/auth/oauth/', include('rest_framework_social_oauth2.urls')),
     re_path(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
     re_path(r'.*', TemplateView.as_view(template_name='index.html')),
 
 ]
