f4fb96cbd323106b1fe2bf65bdbfbc8600a5d508
majklfly <majklfly@gmail.com>
2020-07-25 10:26:19 +0100

    added build to repo

---
+++ build/precache-manifest.65de1e6584d933526dcdb9ea7d1bb8ca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae56cdb0417ea5c41f9472899391f874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e3803759400f7e6410b",
+    "url": "/static/css/2.e811e4a4.chunk.css"
+  },
+  {
+    "revision": "f798a71faaac43c1c397",
+    "url": "/static/css/main.1d25b1ac.chunk.css"
+  },
+  {
+    "revision": "9e3803759400f7e6410b",
+    "url": "/static/js/2.95a687a0.chunk.js"
+  },
+  {
+    "revision": "bf77572f5b4cdfaeaeec164067d2538f",
+    "url": "/static/js/2.95a687a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f798a71faaac43c1c397",
+    "url": "/static/js/main.688a74c6.chunk.js"
+  },
+  {
+    "revision": "79ef570ada1ce0917392",
+    "url": "/static/js/runtime-main.9a8ca20a.js"
+  },
+  {
+    "revision": "f712b83bbd53c6d7ab1b9968efbdde01",
+    "url": "/static/media/DangerZoneLined.f712b83b.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "134f3edd8bcffd435438131e644f5e7c",
+    "url": "/static/media/icon.134f3edd.jpeg"
+  }
+]);
